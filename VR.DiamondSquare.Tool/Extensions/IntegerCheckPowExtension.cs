793609a172ae1bd808524a499d20a19805d6619a
volodymyr.rud <volodymyr.rud@LWO1-LHP-A06047>
2022-09-08 17:49:45 +0300

    Fixed name of extension class.

--- VR.DiamondSquare.Tool/Extensions/IntegerCheckPowExtension.cs
+++ VR.DiamondSquare.Tool/Extensions/IntegerCheckPowExtension.cs
@@ -1,6 +1,6 @@
 ﻿namespace VR.DiamondSquare.Tools.Extensions;
 
-public static class IntegerCheckPowExtension
+public static class IntegerCheckIsPowerOfTwoExtension
 {
     public static bool IsPowerOfTwo(this int x) => (x != 0) && ((x & (x - 1)) == 0);
 }
