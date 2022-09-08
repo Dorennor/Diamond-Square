63b35415a43a8b3759ccabbc8415c0a806d70645
volodymyr.rud <volodymyr.rud@LWO1-LHP-A06047>
2022-09-08 17:36:37 +0300

    New method of converting bitmap to source.

--- VR.DIamondSquare.Tool/Extensions/BitmapImageSourceExtension.cs
+++ VR.DIamondSquare.Tool/Extensions/BitmapImageSourceExtension.cs
@@ -1,41 +1,44 @@
 ﻿using System.Drawing;
-using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace VR.DiamondSquare.Tool.Extensions
 {
     public static class BitmapImageSourceExtension
     {
-        public static BitmapImage GetImageSource(this Bitmap bitmap)
-        {
-            using (MemoryStream memory = new MemoryStream())
-            {
-                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                memory.Position = 0;
-
-                BitmapImage bitmapimage = new BitmapImage();
-
-                bitmapimage.BeginInit();
-                bitmapimage.StreamSource = memory;
-                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapimage.EndInit();
-
-                return bitmapimage;
-            }
-        }
-
-        //[DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
-        //[return: MarshalAs(UnmanagedType.Bool)]
-        //public static extern bool DeleteObject([In] IntPtr hObject);
-
-        //public static ImageSource GetImageSource(this Bitmap bmp)
+        //public static BitmapImage GetImageSource(this Bitmap bitmap)
         //{
-        //    var handle = bmp.GetHbitmap();
-        //    try
+        //    using (MemoryStream memory = new MemoryStream())
         //    {
-        //        return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        //        bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+        //        memory.Position = 0;
+
+        //        BitmapImage bitmapimage = new BitmapImage();
+
+        //        bitmapimage.BeginInit();
+        //        bitmapimage.StreamSource = memory;
+        //        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+        //        bitmapimage.EndInit();
+
+        //        return bitmapimage;
         //    }
-        //    finally { DeleteObject(handle); }
         //}
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public static ImageSource GetImageSource(this Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
     }
 }
