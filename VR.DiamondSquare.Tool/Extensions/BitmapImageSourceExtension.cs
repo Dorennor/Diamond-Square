22fa11d56488883325b869bbe0af2ccfd3ef5a8e
volodymyr.rud <volodymyr.rud@LWO1-LHP-A06047>
2022-09-09 10:07:00 +0300

    Fixed new bitmap to source extension method.

--- VR.DiamondSquare.Tool/Extensions/BitmapImageSourceExtension.cs
+++ VR.DiamondSquare.Tool/Extensions/BitmapImageSourceExtension.cs
@@ -1,5 +1,4 @@
 ﻿using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
@@ -9,35 +8,10 @@ namespace VR.DiamondSquare.Tool.Extensions;
 
 public static class BitmapImageSourceExtension
 {
-    //public static BitmapImage GetImageSource(this Bitmap bitmap)
-    //{
-    //    using (MemoryStream memory = new MemoryStream())
-    //    {
-    //        bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-    //        memory.Position = 0;
-
-    //        BitmapImage bitmapimage = new BitmapImage();
-
-    //        bitmapimage.BeginInit();
-    //        bitmapimage.StreamSource = memory;
-    //        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-    //        bitmapimage.EndInit();
-
-    //        return bitmapimage;
-    //    }
-    //}
-
-    [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool DeleteObject([In] IntPtr hObject);
-
     public static ImageSource GetImageSource(this Bitmap bmp)
     {
         var handle = bmp.GetHbitmap();
-        try
-        {
-            return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-        }
-        finally { DeleteObject(handle); }
+
+        return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
     }
 }
